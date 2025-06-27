3a16f3fb990740d43a6702bc63b0c18c322555f4
Pham Khoa <khoaa8hbtacc2@gmail.com>
2025-06-27 13:37:10 +0700

    add TopNav

---
+++ TopNav/script.js
@@ -0,0 +1,17 @@
+function toggleNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+document.querySelectorAll(".topnav a:not(.icon)").forEach(function (link) {
+  link.addEventListener("click", function () {
+    document.querySelectorAll(".topnav a").forEach(function (a) {
+      a.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
